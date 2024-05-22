c7b17736b95177eac3b92210663439c1aaab5c10
Ernst Stolz <eric@spiritsdontfly.com>
2024-05-22 10:30:20 +0200

    add versionKey: false

--- model/Notes.js
+++ model/Notes.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 /* Definition of what a document looks like */
-const noteSchema = new Schema({
-  content: { type: String, required: true },
-});
+const noteSchema = new Schema(
+  {
+    content: { type: String, required: true },
+  },
+  { versionKey: false }
+);
 
 const Note = mongoose.models.Note || mongoose.model("Note", noteSchema);
 
